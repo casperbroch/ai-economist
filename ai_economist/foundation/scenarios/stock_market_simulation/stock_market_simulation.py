fcd8b87d98792bd23283eb13d267829adb19222e
Casper Bröcheler <85459602+casperbroch@users.noreply.github.com>
2024-05-21 11:01:39 +0200

    Update stock_market_simulation.py

--- ai_economist/foundation/scenarios/stock_market_simulation/stock_market_simulation.py
+++ ai_economist/foundation/scenarios/stock_market_simulation/stock_market_simulation.py
@@ -74,7 +74,7 @@ class StockMarketSimulation(BaseEnvironment):
         self.random_stock_crash_start = np.random.randint(10, 90)
         
         # Set initial crash intensity (between 10% and 30%)
-        self.intensity_crash = np.random.uniform(0.7, 0.9)
+        self.intensity_crash = np.random.uniform(0.8, 0.9)
         
         # Set duration of crash (between 1 and 10 days)
         self.duration_crash = np.random.randint(2, 4)
