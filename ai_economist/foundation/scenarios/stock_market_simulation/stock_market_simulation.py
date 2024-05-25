912d58a51270c689b930212a3007167ec5ec9cae
Casper Bröcheler <85459602+casperbroch@users.noreply.github.com>
2024-05-25 17:17:24 +0200

    Update stock_market_simulation.py

--- ai_economist/foundation/scenarios/stock_market_simulation/stock_market_simulation.py
+++ ai_economist/foundation/scenarios/stock_market_simulation/stock_market_simulation.py
@@ -135,13 +135,10 @@ class StockMarketSimulation(BaseEnvironment):
         
         able_to_trade = self.world.agents[0].state["endogenous"]["AbleToBuy"]
         
-        if self.crash == True and able_to_trade == 0:
-            print("we are unable to trade and crash happening, price is now", self.market.getprice())
-        
         # We check if a crash is currently happening
         if self.crash == True:
             if able_to_trade == 1:        
-                crash_percentage = self.intensity_crash * np.random.uniform(0.3, 0.7)
+                crash_percentage = 0.5#self.intensity_crash * np.random.uniform(0.3, 0.7)
                 self.market.price = self.market.getprice() - (self.market.getprice() * crash_percentage)
 
             self.duration_crash -= 1
