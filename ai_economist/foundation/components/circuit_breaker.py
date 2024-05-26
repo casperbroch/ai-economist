51649b1753c8d00aba4ac51d4cd08a9ad965db91
Casper Bröcheler <85459602+casperbroch@users.noreply.github.com>
2024-05-26 13:53:08 +0200

    test

--- ai_economist/foundation/components/circuit_breaker.py
+++ ai_economist/foundation/components/circuit_breaker.py
@@ -41,9 +41,7 @@ class ExecCircuitBreaker(BaseComponent):
 
     def component_step(self):
         planner_action = self.world.planner.get_component_action(self.name)
-        if planner_action == 0:
-            print("stopped market!! at !!", self.world.timestep)
- 
+        
         if 0 <= planner_action <= self.no_actions: # Make sure the planner action is legal        
             if planner_action == 0:
                 if (self.world.timestep - 1) % self.policy_interval == 0:
