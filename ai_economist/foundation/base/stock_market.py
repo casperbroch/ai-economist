2f71b7cfffeafa3ad809d8ddfb3589fed218575e
Casper Bröcheler <85459602+casperbroch@users.noreply.github.com>
2024-06-03 16:41:00 +0200

    Update stock_market.py

--- ai_economist/foundation/base/stock_market.py
+++ ai_economist/foundation/base/stock_market.py
@@ -23,9 +23,7 @@ class StockMarket:
         else:
             p = 0
             
-        print("p",p)
-        print("rr",random_return)
-        self.price = self.price * (1 + (0.5*random_return + 0.05*p))
+        self.price = self.price * (1 + (0.5*random_return + 0.001*p))
         
         
     def nextsteprandom(self):
